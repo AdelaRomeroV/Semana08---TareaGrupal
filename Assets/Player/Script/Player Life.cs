85f384cd7b12e22b18d3e9d339c4ca0436310043
AdelaRomeroV <ajrvjarinee17@gmail.com>
2024-05-25 00:30:45 -0500

    Adela Romero - Código de vida en el jugador y enemigo, código de daño

---
+++ Assets/Player/Script/Player Life.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLife : Life
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("BulletEnemy"))
+        {
+            ChangeLife(-other.gameObject.gameObject.GetComponent<Damage>().GetDamage());
+            Destroy(other.gameObject);
+        }
+
+    }
+}
---
+++ Assets/Script/Enemy/Script/Enemy Life.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLife : Life
+{ 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            ChangeLife(-other.gameObject.gameObject.GetComponent<Damage>().GetDamage());
+            Destroy(other.gameObject);
+        }
+
+    }
+}
